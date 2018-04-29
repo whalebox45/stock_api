f87377fc1156c44ddba088b75a84b66beda12143
whale <whalebox4.5@gmail.com>
2018-04-29 20:36:06 +0800

    first

---
+++ mysql_query.js
@@ -0,0 +1,15 @@
+var connection = require('./mysql_config');
+connection.connect();
+
+security_code = "1234";
+
+connection.query("select date ,security_code ,name ," +
+    "open_price ,highest_price ,lowest_price ,closing_price " +
+    "from stock_2018"+" where security_code = " + security_code + ";",
+    function(err, row, fields) {
+      if (err) throw err;
+      console.log(JSON.stringify(row));
+    }
+);
+
+connection.end();
