0110e93f9037d015a43534924c9543c2199c5c35
whale <whalebox4.5@gmail.com>
2018-05-04 22:33:04 +0800

    add mysql to serv_app.js

--- serv_app.js
+++ serv_app.js
@@ -1,21 +1,27 @@
 var express = require('express');
-var app = express();
 var fs = require("fs");
 var bodyParser = require('body-parser');
+
+var app = express();
+
+var mysql = require("./mysql_config");
+var db = mysql.db;
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.post('/ohlc:stockid', function(req,res){
-
-
-})
-
-app.get('/listUsers', function (req, res) {
-   fs.readFile("./" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+app.post('/ohlc/:stockid', function(req,res){
+    security_code = req.params.stockid;
+    db.query('select date, security_code, name'+
+    'open_price, highest_price, lowest_price, closing_price '+
+    'from stock_2018 where security_code=? ;',[security_code],
+    function(err,row,fields){
+        if(err) throw err;
+        console.log(JSON.stringify(row));
+    })
+    res.end(req.params.stockid);
+    
 })
 
 app.get('/listUser/:uuid', function (req, res) {
@@ -29,38 +35,12 @@ app.get('/listUser/:uuid', function (req, res) {
 })
 
 
-app.post('/addUser', function (req, res) {
-     var uuid  = req.body.uuid;
-     var uname = req.body.uname;
-     var prof  = req.body.prof;
-     var passwd = req.body.passwd;
-     var str = 'Name: '+ uname +', PROF: '+prof+' UUID: '+uuid+' PASS: '+ passwd;
-     console.log( str );
-     res.end(str);
-})
-
-app.put('/updateUser', function (req, res) {
-   var uuid  = req.body.uuid;
-     var uname = req.body.uname;
-     var prof  = req.body.prof;
-     var passwd = req.body.passwd;
-     var str = 'UPDATE: Name: '+ uname +', PROF: '+prof+' UUID: '+uuid+' PASS: '+ passwd;
-     console.log( str );
-     res.end(str);
-})
-
-app.delete('/deleteUser/:id', function (req, res) {
-
-})
-
-
-
 var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Web app listening at http://%s:%s", host, port)
 
 })
 
