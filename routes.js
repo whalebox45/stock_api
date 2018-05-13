f345c2e96109fc167c2014977868410121891725
whale <whalebox4.5@gmail.com>
2018-05-13 22:25:31 +0800

    fix code

--- routes.js
+++ routes.js
@@ -5,9 +5,9 @@ var db = mysql.db;
 module.exports = function(app){
 	app.get('/pe_ratio', function(req,res){
 		console.log('got the get');
-		db.query('SELECT date,security_code, name,pe_ratio FROM stock_eagle.pepb_2018'+
-		'where date = (SELECT distinct date from pepb_2018 order by date desc limit 1) and pe_ratio != 0'+
-		'order by cast(pe_ratio as DECIMAL(5,2)) limit 10;',
+		db.query('SELECT date, security_code, name, pe_ratio FROM stock_eagle.pepb_2018 '+
+		'WHERE date = (SELECT distinct date FROM pepb_2018 ORDER BY date DESC LIMIT 1) AND pe_ratio != 0 '+
+		'ORDER BY cast(pe_ratio as DECIMAL(5,2)) LIMIT 10;',
 		function(err,row,fields){
 			if(err) throw err;
 			res.end(JSON.stringify(row));
