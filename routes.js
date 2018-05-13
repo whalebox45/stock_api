e4f3344902463d35fe815d9eca310ab589f5317e
whale <whalebox4.5@gmail.com>
2018-05-13 22:20:27 +0800

    add pe_ratio query

--- routes.js
+++ routes.js
@@ -5,14 +5,21 @@ var db = mysql.db;
 module.exports = function(app){
 	app.get('/pe_ratio', function(req,res){
 		console.log('got the get');
-		res.end();
+		db.query('SELECT date,security_code, name,pe_ratio FROM stock_eagle.pepb_2018'+
+		'where date = (SELECT distinct date from pepb_2018 order by date desc limit 1) and pe_ratio != 0'+
+		'order by cast(pe_ratio as DECIMAL(5,2)) limit 10;',
+		function(err,row,fields){
+			if(err) throw err;
+			res.end(JSON.stringify(row));
+		})
 	});
 
 	app.get('/ohlc/:stockid', function(req,res){
 		security_code = req.params.stockid;
 		db.query('select date, security_code, name,'+
 		'open_price, high_price, low_price, close_price '+
-		'from stock_2018 where security_code=? ;',[security_code],
+		'from stock_2018 where security_code=? ;',
+		[security_code],
 		function(err,row,fields){
 			if(err) throw err;
 			res.end(JSON.stringify(row));
