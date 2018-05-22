d2d3f825c4f3b9f370a12fa6c1580768e9827e4f
whale <whalebox4.5@gmail.com>
2018-05-23 00:02:41 +0800

    csrf

--- routes.js
+++ routes.js
@@ -67,6 +67,7 @@ module.exports = function (app) {
 	
 	app.get('/json_test', function (req, res) {
 		res.set({ 'content-type': 'application/json; charset=utf-8' });
+		res.render('send', { csrfToken: req.csrfToken() })
 		res.end(JSON.stringify({ "test": 'ok' }));
 		console.log(req.body);
 	})
