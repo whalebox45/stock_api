c3f4391d0e9c0cd53956215623bfeb0f32fe8b94
whale <whalebox4.5@gmail.com>
2018-05-23 00:06:31 +0800

    s

--- serv_app.js
+++ serv_app.js
@@ -9,6 +9,13 @@ var csrfMiddleware = csurf({
   cookie: true
 });
 
+var allowCrossDomain = function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
 
 var app = express();
 
