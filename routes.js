e74e075871d807f91ebea3da815e1a3da15ac85d
whale <whalebox4.5@gmail.com>
2018-05-23 21:34:29 +0800

    wh

--- routes.js
+++ routes.js
@@ -71,8 +71,8 @@ module.exports = function (app) {
 
 	app.get('/json_test', function (req, res) {
 		res.set({ 'content-type': 'application/json; charset=utf-8' });
-		res.end(JSON.stringify({ "test": 'ok' }));
 		res.set("Connection","close");
+		res.end(JSON.stringify({ "test": 'ok' }));
 		console.log(req.body);
 	})
 
